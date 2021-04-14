ad2dbc4529d8367f737d0d7f7508000a52359453
MacdonEm <29071760+MacdonEm@users.noreply.github.com>
2021-04-14 17:16:27 -0400

    Added timestamp for easier logging

--- ai_iso_test/creation.sh
+++ ai_iso_test/creation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-######## Passing arguments ########
+######## Setting variables ########
 if [ -z "$3" ]; then
 echo 'To use: ./aiWorkload.sh CLUSTER_NAME BASE_DOMAIN MACHINE_NETWORK_CIDR PULL_SECRET SSH_KEY_PRIV SSH_KEY_PUB'
 exit 1
@@ -12,6 +12,7 @@ MACHINE_NETWORK_CIDR=$3
 PULL_SECRET=`cat $4 | base64 -w0`
 SSH_KEY_PRIV=`cat $5 | base64 -w0`
 SSH_KEY_PUB=`cat $6`
+D=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 
 echo "Public Key Provided:"
 echo $SSH_KEY_PUB
@@ -21,15 +22,13 @@ for i in `seq 1 1`; do
   printf "\n"
   echo "Beginning Deployment Number $i"
   echo "------------------------------"
-  printf "\n\n\n"
+  printf "\n"
 
   ######## Set CLUSTER_NAME ########
   CLUSTER_NAME=$CL_NM$i
 
   ######## Apply creation manuscript ########
-  echo "Creating Cluster Deployments..."
-  
-  echo "Applying creation manuscript..."
+  echo "$D : Applying creation manuscript..."
   cat manuscript.yaml | \
   sed "s/CLUSTER_NAME/$CLUSTER_NAME/g" | \
   sed "s/BASE_DOMAIN/$BASE_DOMAIN/g" | \
@@ -38,25 +37,10 @@ for i in `seq 1 1`; do
   sed "s~PULL_SECRET~'$PULL_SECRET'~g" | \
   sed "s~SSH_KEY_PRIV~'$SSH_KEY_PRIV'~g" | \
   oc apply -f -
-
-#  for j in `seq 1 30`; do
-#    export DISCOVERY_ISO_URL=`oc get installenv $CLUSTER_NAME -n $CLUSTER_NAME -ojsonpath='{.status.isoDownloadURL}'`
-#    if [ ! -z $DISCOVERY_ISO_URL ]; then
-#      echo "ISO retrieved!"
-#      break
-#    fi
-#  done
-#  echo "ISO download: $DISCOVERY_ISO_URL"
-
-  ######## Generate rootfs ########
-#  echo "Generating rootfs..."
-#  export ASSISTED_SERVICE=`oc get route -n assisted-installer assisted-service -ojsonpath='{.spec.host}'`
-#  export OPENSHIFT_VERSION=4.8
-#  export ROOTFS_ISO_URL="http://$ASSISTED_SERVICE/api/assisted-install/v1/boot-files?file_type=rootfs.img&openshift_version=$OPENSHIFT_VERSION"
-#  echo "Rootfs download: $ROOTFS_ISO_URL"
+  echo "$D : Manuscript applied!!"
 
   ######## Current Deployment Number ########
-  printf "\n\n\n"
+  printf "\n"
   echo "------------------------------"
   echo "Deployment Number $i Completed"
 done
