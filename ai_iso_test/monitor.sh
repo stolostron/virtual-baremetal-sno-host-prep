68e8829d850b99f90bb3b872809bb1decd49b7e7
MacdonEm <29071760+MacdonEm@users.noreply.github.com>
2021-04-15 14:02:35 -0400

    Monitor script functional.

--- ai_iso_test/monitor.sh
+++ ai_iso_test/monitor.sh
@@ -5,37 +5,37 @@ file=managedcluster.csv
 if [ ! -f ${file} ]; then
   echo "\
 date,\
-clusterdeployment_ct,\
-installenv_ct,\
-iso_generated,\
+clusterdeployment_count,\
+installenv_count,\
+iso_generated\
 " > ${file}
 fi
 
-export HISTO_DELAY=${HISTO_DELAY:-60}
+export HISTO_DELAY=${HISTO_DELAY:-20}
 echo "DELAY: ${HISTO_DELAY}"
 COUNTER=0
 
 while [ true ]; do
   D=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 
-  clusterdeployment_status=$(oc get clusterdeployment -A -o custom-columns=NAME:metadata.name --no-headers)
-  clusterdeployment_ct=$(echo $clusterdeployment_status | wc -l)
+  clusterdeployment_status=$(oc get clusterdeployment -A -o custom-columns=NAME:metadata.name --no-headers | base64 -w 0)
+  clusterdeployment_count=$(echo $clusterdeployment_status | base64 --decode | wc -l | tr -d " ")
   
-  installenv_status=$(oc get installenv -A -o custom-columns=NAME:metadata.name,ISO:status.isoDownloadURL --no-headers)
-  intsallenv_ct=$(echo $installenv_status | wc -l)
-  iso_generated=$(echo $installenv_status | grep -v none | wc -l)
+  installenv_status=$(oc get installenv -A -o custom-columns=NAME:metadata.name,ISO:status.isoDownloadURL --no-headers | base64 -w 0)
+#  intsallenv_count=$(echo $installenv_status | base64 --decode | wc -l | tr -d " ")
+  iso_generated=$(echo $installenv_status    | base64 --decode | grep -v none | wc -l | tr -d " ")
 
   echo $D
-  echo "$D clusterdeployment_ct      : $clusterdeployment_ct"
-
-  echo "$D installenv_ct      : $installenv_ct"
-  echo "$D iso_generated      : $iso_generated"
+  echo "$D   clusterdeployment_count      : $clusterdeployment_count"
+#  echo "$D   installenv_count             : $installenv_count"
+  echo "$D   installenv_count             : $(echo $installenv_status | base64 --decode | wc -l | tr -d " ")"
+  echo "$D   iso_generated                : $iso_generated"
 
   echo "\
 $D,\
-$clusterdeployment_ct,\
-$installenv_ct,\
-$iso_generated,\
+$clusterdeployment_count,\
+$(echo $installenv_status | base64 --decode | wc -l | tr -d " "),\
+$iso_generated\
 " >> managedcluster.csv
 
   sleep $HISTO_DELAY
