3e939992e48c45f827a4e7c42c4677e9e2be7b13
Tara Gu <tgu@redhat.com>
2021-04-22 11:04:57 -0400

    working (semi)

--- clusterdeployment/create-resources.sh
+++ clusterdeployment/create-resources.sh
@@ -15,24 +15,26 @@ fi
 KUBECONFIG=/root/bm/kubeconfig
 
 pull_secret_base64=`cat $pull_secret_path | base64 -w 0`
-public_key_base64=`cat "${ssh_key_path}.pub" | base64 -w 0`
+public_key=`cat "${ssh_key_path}.pub"`
 private_key_base64=`cat "${ssh_key_path}" | base64 -w 0`
 
 cluster_array=`awk -F"," '{print $1}' inventory-manifest.csv`
 
 input=inventory-manifest.csv
-while  IFS="," read cluster_name base_domain mac_addr ip_addr gateway machine_network_cidr public_ip_network_prefix bmc_addr bmc_username_base64 bmc_password_base64 ; do
+
+sed 1d $input | while  IFS="," read cluster_name base_domain mac_addr ip_addr public_ip_network_prefix gateway machine_network_cidr bmc_addr bmc_username_base64 bmc_password_base64 ; do
     # TODOTARA 1) apply at a rate and 2) refactor single cluster creation
 
     echo "=============== creating resources for cluster $cluster_name ==============="
+    echo "$cluster_name, $base_domain, $mac_addr, $ip_addr, $gateway, $machine_network_cidr, $public_ip_network_prefix, $bmc_addr, $bmc_username_base64, $bmc_password_base64"
 
     yaml_dir=`echo $cluster_name/manifest`
     mkdir -p $yaml_dir
 
     cat clusterdeployment.yaml.template | \
 	sed -e s/cluster_name/$cluster_name/g \
-	    -e s/public_key_base64/$public_key_base64/g \
-	    -e s/machine_network_cidr/$machine_network_cidr\g \
+	    -e "s~public_key~'$public_key'~g" \
+	    -e s~machine_network_cidr~$machine_network_cidr~g \
             -e s/base_domain/$base_domain/g > $yaml_dir/500-clusterdeployment.yaml
 
     cat bmh-secret.yaml.template | \
@@ -42,8 +44,8 @@ while  IFS="," read cluster_name base_domain mac_addr ip_addr gateway machine_ne
 
     cat infraenv.yaml.template | \
 	sed -e s/cluster_name/$cluster_name/g \
-            -e s/public_key_base64/$public_key_base64/g > $yaml_dir/800-infraenv.yaml
-
+            -e "s~public_key~'$public_key'~g" > $yaml_dir/800-infraenv.yaml
+    
     cat managedcluster.yaml.template | \
 	sed -e s/cluster_name/$cluster_name/g > $yaml_dir/700-managedcluster.yaml
 
@@ -52,11 +54,11 @@ while  IFS="," read cluster_name base_domain mac_addr ip_addr gateway machine_ne
 
     cat nmstate.yaml.template | \
 	sed -e s/cluster_name/$cluster_name/g \
-	    -e s/dns_resolver/$dns_resolver/g \
-	    -e s/ip_addr/$ip_addr/g \
-	    -e s/mac_addr/$mac_addr/g \
-	    -e s/public_ip_network_prefix/$public_ip_network_prefix/g \
-	    -e s/cluster_name/$cluster_name/g > $yaml_dir/300-nmstate.yaml
+	    -e "s~dns_resolver~'$dns_resolver'~g" \
+	    -e "s~ip_addr~'$ip_addr'~g" \
+	    -e "s~mac_addr~'$mac_addr'~g" \
+	    -e "s~gateway~'$gateway'~g" \
+	    -e s/public_ip_network_prefix/$public_ip_network_prefix/g > $yaml_dir/300-nmstate.yaml
 
     cat pull-secret.yaml.template | \
 	sed -e s/cluster_name/$cluster_name/g \
@@ -67,6 +69,7 @@ while  IFS="," read cluster_name base_domain mac_addr ip_addr gateway machine_ne
 	    -e s/private_key_base64/$private_key_base64/g > $yaml_dir/400-private-key.yaml
 
     # Write klusterletaddonconfig
+    echo "======== Write klusterletaddonconfig ========="
     cat klusterletaddonconfig.yaml.template | \
 	sed -e s/cluster_name/$cluster_name/g > $yaml_dir/600-klusterletaddonconfig.yaml
     addon_array=`jq '.acmAddonConfig[].addonName' addon.json`
@@ -75,21 +78,8 @@ while  IFS="," read cluster_name base_domain mac_addr ip_addr gateway machine_ne
 	addonName=$(jq -r ".acmAddonConfig[$k].addonName" addon.json);
 	enabled=$(jq -r ".acmAddonConfig[$k].enabled" addon.json);
 	# Need to write to yaml; cannot use yq because bastion machine doesn't have yq
-	echo -e "\n  $addonName:\n    enabled: $enabled" >> $yaml_dir/klusterletaddonconfig.yaml
+	echo -e "\n  $addonName:\n    enabled: $enabled" >> $yaml_dir/600-klusterletaddonconfig.yaml
     done
-    #for row in $(jq '.acmAddonConfig[]' addon.json); do
-    # jq -rc '.acmAddonConfig[]' addon.json | while IFS='' read stack;do
-    # 	echo $stack
-    # 	name=$(echo "$stack" | jq '.addonName')
-    # 	echo $name
-    # 	status=$(echo "$stack" | jq '.enabled')
-    # 	echo $status
-    # done
-    
-    # 	addon=`jq -r `
-    # 	enabled=`jq '.acmAddonConfig[j].enabled' addon.json`
-    # 	yq w $yaml_dir/klusterletaddonconfig.yaml '.spec.addon_array[j]' $enabled
-    # done
 
     echo "====== about to apply resources (except baremetal host) ======"
     # # Apply resources (except baremetal host) for each cluster:
@@ -112,5 +102,5 @@ while  IFS="," read cluster_name base_domain mac_addr ip_addr gateway machine_ne
     # mkdir -p kubeconfig
     # ./get-kubeconfigs.sh $cluster_name $curr_file_path/kubeconfig
 
-done < $input
+done
 
