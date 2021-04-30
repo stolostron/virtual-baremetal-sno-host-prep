9e3f23f966bd8c2ed0d86b6e1b179cba25959b68
Tara Gu <tgu@redhat.com>
2021-04-30 11:10:44 -0400

    update monitor script and add log script for bmo and assisted-installer

--- clusterdeployment/monitor-deployment.sh
+++ clusterdeployment/monitor-deployment.sh
@@ -4,45 +4,67 @@ set -o nounset
 # Starts monitoring the progress of the creation and provisioning of SNO clusters
 # installed with Assisted Installer.
 # Usage:
-#   ./monitor-deployment.sh
+#   ./monitor-deployment.sh [INTERVAL_SECONDS]
 # The output of the stats will both be displayed in terminal output and saved to
 # csv file managedsnocluster.csv.
+# You can optionally supply the number of seconds between each intervals of collecting
+# stats. The default value is 10 seconds.
 
 # The following stats are reported:
-#   - manifests_applied: the number of baremetal host manifests that have been applied.
-#   - bmh_provisioned: running discovery iso and download rootfs.
-#   - agent_count: rootfs has been downloaded and discovery results sent back to the hub.
+#   - initialized: the number of clusterdeployment that have been created.
+#   - booted: baremetal hosts that are provisioned; currently running discovery iso and downloading rootfs.
+#   - discovered: rootfs has been downloaded and discovery results sent back to the hub; agent created.
+#   - provisioning: clusterdeployment in provisioning state
+#   - completed: clusterdeployment in completed state
+#   - managed: managedcluster avaialble
+#   - agent_installed: managedclusteraddon avaialble
 
 file=managedsnocluster.csv
 
 if [ ! -f ${file} ]; then
   echo "\
 date,\
-manifests_applied,\
-agent_count,\
-bmh_provisioned\
+initialized,\
+booted,\
+discovered,\
+provisioning,\
+completed,\
+managed,\
+agent_installed\
 " > ${file}
 fi
 
 export KUBECONFIG=/root/bm/kubeconfig
-sleep_seconds=5
+sleep_seconds=10
 while true; do
   D=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 
-  manfest_applied=$(oc get clusterdeployment --all-namespaces | grep sno | wc -l | tr -d " ")
-  agent_count=$(oc get agent --all-namespaces | wc -l | tr -d " ")
-  bmh_provisioned_count=$(oc get bmh --all-namespaces | grep -c provisioned | tr -d " ")
+  initialized=$(oc get clusterdeployment --all-namespaces | grep -c sno | tr -d " ")
+  booted=$(oc get bmh --all-namespaces | grep -c provisioned | tr -d " ")
+  discovered=$(oc get agent --all-namespaces | wc -l | tr -d " ")
+  provisioning=$(oc get clusterdeployment --all-namespaces | grep sno | grep provisioning | wc -l | tr -d " ")
+  completed=$(oc get clusterdeployment --all-namespaces | grep sno | grep completed | wc -l | tr -d " ")
+  managed=$(oc get managedcluster --no-headers -o custom-columns=JOINED:'.status.conditions[?(@.type=="ManagedClusterJoined")].status',AVAILABLE:'.status.conditions[?(@.type=="ManagedClusterConditionAvailable")].status' | grep -v none | grep True | grep -v Unknown | wc -l | tr -d " ")
+  agent_installed=$(oc get managedclusteraddon -A --no-headers -o custom-columns=AVAILABLE:'.status.conditions[?(@.type=="Available")].status' | grep -c True)
 
   echo "$D"
-  echo "$D manfest_applied     : $manfest_applied"
-  echo "$D agent_count     : $agent_count"
-  echo "$D bmh_provisioned_count      : $bmh_provisioned_count"
+  echo "$D initialized: $initialized"
+  echo "$D booted: $booted"
+  echo "$D discovered: $discovered"
+  echo "$D provisioning: $provisioning"
+  echo "$D completed: $completed"
+  echo "$D managed: $managed"
+  echo "$D agent_installed: $agent_installed"
 
   echo "\
 $D,\
-$manfest_applied,\
-$agent_count,\
-$bmh_provisioned_count\
+$initialized,\
+$booted,\
+$discovered,\
+$provisioning,\
+$completed,\
+$managed,\
+$agent_installed\
 " >> ${file}
 
   sleep "$sleep_seconds"
