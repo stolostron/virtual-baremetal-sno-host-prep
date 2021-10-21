b51e5361095b2d1040849c917a7fd2c5d96ee5b6
Subbarao Meduri <smeduri@redhat.com>
2021-10-21 15:48:55 -0400

    update scripts to match latest minio and env

--- setup-observability-minio/setup-observability-with-minio.sh
+++ setup-observability-minio/setup-observability-with-minio.sh
@@ -6,6 +6,18 @@ oc create namespace $NS
 # NOTE: step 2 not needed for GA builds
 oc get secret multiclusterhub-operator-pull-secret -n open-cluster-management -o yaml | oc apply -n $NS -f -
 # step 3
+cat > object-storage-data.txt <<EOF
+type: s3
+config:
+  bucket: "thanos"
+  endpoint: "minio.minio.svc.cluster.local:9000"
+  insecure: true
+  access_key: "minio"
+  secret_key: "minio123"
+EOF
+cat object-storage-data.txt
+oc delete secret thanos-object-storage -n $NS
+oc create secret generic thanos-object-storage --from-file=thanos.yaml=./object-storage-data.txt -n $NS
 oc get secret thanos-object-storage -n $NS -o yaml
 # step 4
 cat > multiclusterobservability_cr.yaml <<EOF
@@ -30,3 +42,6 @@ spec:
     storageClass: localstorage-sc # upate if needed
     storeStorageSize: 10Gi
 EOF
+# step 5
+oc project $NS
+oc apply -f multiclusterobservability_cr.yaml
